e64636bd9c0a4bf49f076bb464ec2ca9ff513b21
rianhz <ryanhidayat.rh197@gmail.com>
2022-05-27 11:28:36 +0800

    update contact form using emailjs

--- form.js
+++ form.js
@@ -1,24 +1,11 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbypkKr8gGhKygmyHmR3i135joz8VQzvDR3BtRYd3cFKpgSKEN_1fsmcUJJbZRd42JfE/exec';
-const form = document.forms['contact-port'];
-const btnKirim = document.querySelector('.btn-kirim');
-const btnLoading = document.querySelector('.btn-loading');
-const alrtSuccess = document.querySelector('.alert');
+function SendMail() {
+  var params = {
+    from_name: document.getElementById('nama').value,
+    from_email: document.getElementById('email').value,
+    by_pesan: document.getElementById('pesan').value,
+  };
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  // btn-kirim none, btn-loading tampil
-  btnLoading.classList.toggle('d-none');
-  btnKirim.classList.toggle('d-none');
-  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-    .then((response) => {
-      // btn-loading none, btn-kirim tampil
-      btnLoading.classList.toggle('d-none');
-      btnKirim.classList.toggle('d-none');
-
-      // alert dan finish
-      alrtSuccess.classList.toggle('d-none');
-      form.reset();
-      console.log('Success!', response);
-    })
-    .catch((error) => console.error('Error!', error.message));
-});
+  emailjs.send('service_m6bzei9', 'template_oq1osn8', params).then(function () {
+    alert('Success');
+  });
+}
